e0053990e3a9c07c7ca33efe56e385ed6a5e54e5
Pranjal Agrawal <pranjal.ag.1999@gmail.com>
2021-10-15 23:37:41 +0530

    Update

--- hyper.js
+++ hyper.js
@@ -6,7 +6,7 @@ module.exports = {
   config: {
     // choose either `'stable'` for receiving highly polished,
     // or `'canary'` for less polished but more frequent updates
-    updateChannel: "stable",
+    //updateChannel: "stable",
 
     // default font size in pixels for all tabs
     fontSize: 12,
@@ -59,7 +59,7 @@ module.exports = {
     borderColor: "#333",
 
     // custom CSS to embed in the main window
-    css: "",
+    css: ".notification_indicator{display:none;}",
 
     // custom CSS to embed in the terminal window
     termCSS: "",
