c0574928256416deb2d0c868bc1c7d8e7528876b
Pranjal Agrawal <pranjal.ag.1999@gmail.com>
2022-09-30 11:24:34 +0530

    Update

--- nvim/lua/config/bufferline.lua
+++ nvim/lua/config/bufferline.lua
@@ -12,6 +12,7 @@ function M.setup()
       show_buffer_close_icons = true,
       show_close_icon = false,
       offsets = { { filetype = "neo-tree", text = "File Explorer", highlight = "Directory", text_align = "center" } },
+      sort_by = "insert_after_current"
     },
   }
 
