aed2a52d4a2d532fc51e029db50dbf830a5763aa
Pranjal Agrawal <pranjal.ag.1999@gmail.com>
2022-10-01 11:36:40 +0530

    Update

--- nvim/after/plugin/defaults.lua
+++ nvim/after/plugin/defaults.lua
@@ -35,9 +35,9 @@ vim.cmd [[
 
 -- vim.opt.foldmethod = "indent" -- Fold by indent
 -- vim.opt.foldcolumn = "1"
-vim.opt.foldlevel = 99
+-- vim.opt.foldlevel = 99
 -- vim.opt.foldlevelstart = -1
-vim.opt.foldenable = true
+-- vim.opt.foldenable = true
 
 -- For termguicolors
 vim.cmd [[
