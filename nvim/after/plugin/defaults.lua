3a56e6005871fec94ccda8bdc98018ebba2073d3
Pranjal Agrawal <pranjal.ag.1999@gmail.com>
2022-10-25 11:41:18 +0530

    Update

--- nvim/after/plugin/defaults.lua
+++ nvim/after/plugin/defaults.lua
@@ -59,7 +59,8 @@ vim.cmd [[
 -- For Neovide
 vim.cmd [[
   if exists("g:neovide")
-    set guifont=Monaco:h11
+    " set guifont=Monaco:h11
+    set guifont=CaskaydiaCove\ NF\ Mono:h12
     let g:neovide_cursor_unfocused_outline_width=0.125
     let g:neovide_cursor_animation_length=0.02
     endif
