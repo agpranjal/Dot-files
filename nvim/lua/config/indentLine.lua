1b70a6b9d2ee3775e9e64a5d597e721a28db45ad
Pranjal Agrawal <pranjal.ag.1999@gmail.com>
2023-03-28 23:35:01 +0530

    Update

--- nvim/lua/config/indentLine.lua
+++ nvim/lua/config/indentLine.lua
@@ -3,7 +3,8 @@ local M = {}
 function M.setup()
   vim.cmd [[
     let g:indentLine_setColors = 0
-      let g:indentLine_char_list = ['|', '¦', '┆', '┊']
+    " let g:indentLine_char_list = ["▏"]
+    let g:indentLine_char_list = ["│"]
   ]]
 end
 
