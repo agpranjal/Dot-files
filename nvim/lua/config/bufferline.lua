9c7aced2b91738623f3cfb0ff79594ce3f1bd0a8
Pranjal Agrawal <pranjal.ag.1999@gmail.com>
2023-07-07 00:53:13 +0530

    Update

--- nvim/lua/config/bufferline.lua
+++ nvim/lua/config/bufferline.lua
@@ -9,7 +9,7 @@ function M.setup()
       diagnostics = "nvim_lsp",
       -- separator_style = "slant" or "padded_slant",
       show_tab_indicators = true,
-      show_buffer_close_icons = true,
+      show_buffer_close_icons = false,
       show_close_icon = false,
       offsets = { { filetype = "neo-tree", text = "File Explorer", highlight = "Directory", text_align = "center" } },
       sort_by = "insert_after_current"
