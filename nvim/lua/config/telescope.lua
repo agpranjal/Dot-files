4b2145dae2a38e84de43398336bcd8ea3c59da16
Pranjal Agrawal <pranjal.ag.1999@gmail.com>
2023-07-15 01:10:22 +0530

    Update

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -40,7 +40,7 @@ function M.setup()
 
   -- Load Telescope extensions
   telescope.load_extension "fzf"
-  -- telescope.load_extension "repo"
+  telescope.load_extension "repo"
   telescope.load_extension "file_browser"
   telescope.load_extension "projects" -- project.nvim
   -- telescope.load_extension "software-licenses"
