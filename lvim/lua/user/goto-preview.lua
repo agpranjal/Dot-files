543491bf0efcb0520a135c07d7dec70c356ebf76
Pranjal Agrawal <pranjal.ag.1999@gmail.com>
2023-10-17 00:32:50 +0530

    Update

--- lvim/lua/user/goto-preview.lua
+++ lvim/lua/user/goto-preview.lua
@@ -1,8 +1,13 @@
 require('goto-preview').setup {
-  width = 120,                   -- Width of the floating window
-  height = 25,                   -- Height of the floating window
-  default_mappings = true,       -- Bind default mappings
-  debug = false,                 -- Print debug information
-  opacity = nil,                 -- 0-100 opacity level of the floating window where 100 is fully transparent.
-  post_open_hook = nil           -- A function taking two arguments, a buffer and a window to be ran as a hook.
+  width = 120,             -- Width of the floating window
+  height = 25,             -- Height of the floating window
+  default_mappings = true, -- Bind default mappings
+  debug = false,           -- Print debug information
+  opacity = nil,           -- 0-100 opacity level of the floating window where 100 is fully transparent.
+  post_open_hook = nil     -- A function taking two arguments, a buffer and a window to be ran as a hook.
 }
+
+vim.keymap.set(
+  "n", "gp", "<cmd>lua require('goto-preview').close_all_win()<CR>",
+  { noremap = true, desc = "Close Preview" }
+)
