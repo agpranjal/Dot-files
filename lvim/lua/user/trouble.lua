d2ceeeeae2e0af18e443405653f1219329d280da
Pranjal Agrawal <pranjal.ag.1999@gmail.com>
2023-10-17 01:36:01 +0530

    Update

--- lvim/lua/user/trouble.lua
+++ lvim/lua/user/trouble.lua
@@ -5,12 +5,12 @@ require "trouble".setup {
     close = "q"
   }
 }
-lvim.builtin.which_key.mappings["t"] = {
+lvim.builtin.which_key.mappings.d = {
   name = "Diagnostics",
   -- t = { "<cmd>TroubleToggle<cr>", "trouble" },
-  w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "workspace" },
-  d = { "<cmd>TroubleToggle document_diagnostics<cr>", "document" },
+  w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "Workspace Diagnostics" },
+  d = { "<cmd>TroubleToggle document_diagnostics<cr>", "Document Diagnostics" },
   q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
   l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
-  r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
+  r = { "<cmd>TroubleToggle lsp_references<cr>", "Current Word References" },
 }
