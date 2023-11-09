9b7a8bbff4c43a1425df48e27a97c9a3a754386d
Pranjal Agrawal <pranjal.ag.1999@gmail.com>
2023-11-10 02:38:05 +0530

    Update

--- lvim/lua/user/mappings.lua
+++ lvim/lua/user/mappings.lua
@@ -172,16 +172,19 @@ vim.cmd [[
 let g:VM_default_mappings = 0
 let g:VM_maps = {}
 
+
 let g:VM_maps['Find Under']                  = '<C-n>'
 let g:VM_maps['Find Subword Under']          = '<C-n>'
-let g:VM_maps["Select All"]                  = '\\A'
+" let g:VM_maps["Select All"]                  = '\\A'
+let g:VM_maps["Align"]                  = '\\A'
+let g:VM_maps["Select All"]                  = '\\a'
 let g:VM_maps["Start Regex Search"]          = '\\/'
 let g:VM_maps["Add Cursor Down"]             = '<C-Down>'
 let g:VM_maps["Add Cursor Up"]               = '<C-Up>'
 let g:VM_maps["Add Cursor At Pos"]           = '\\\'
 
 let g:VM_maps["Visual Regex"]                = '\\/'
-let g:VM_maps["Visual All"]                  = '\\A'
+" let g:VM_maps["Visual All"]                  = '\\A'
 let g:VM_maps["Visual Add"]                  = '\\a'
 let g:VM_maps["Visual Find"]                 = '\\f'
 let g:VM_maps["Visual Cursors"]              = '\\c'
